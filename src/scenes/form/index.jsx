f3f62dfcd70739fe779bffe04508a013817de5ff
vishnu-rbo <vishnuashvika@gmail.com>
2023-11-07 10:03:38 +0530

    added final11

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -180,6 +180,7 @@ const Form = () => {
           justifyContent:"center",
           alignItems:"center",
           mt: '0px',
+          padding: 25,
           width: '100%',
           height: "100%",
           bgcolor: colors.blue[300]}
